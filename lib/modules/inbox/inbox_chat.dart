07409f5cf5a87eba40f2702f746b4badb59c983b
Nguyễn Quang Huy <quanghuy1998kh@gmail.com>
2021-01-13 13:46:17 +0700

    update loadmore animation

--- lib/modules/inbox/inbox_chat.dart
+++ lib/modules/inbox/inbox_chat.dart
@@ -7,7 +7,7 @@ import 'package:petland/modules/inbox/inbox_model.dart';
 import 'package:petland/share/import.dart';
 import 'package:dash_chat/dash_chat.dart';
 
-import 'load_more_scroll_up.dart';
+import 'load_more.dart';
 
 class InboxChat extends StatefulWidget {
   final FbInboxGroupModel group;
@@ -30,6 +30,9 @@ class _InboxChatState extends State<InboxChat> {
   InboxBloc _inboxBloc;
   AuthBloc _authBloc;
   ScrollController scrollController = ScrollController();
+  bool shouldShowLoadEarlier = true;
+  bool reachEndList = false;
+  bool onLoadMore = false;
 
   List<ChatMessage> messages = List<ChatMessage>();
 
@@ -79,9 +82,23 @@ class _InboxChatState extends State<InboxChat> {
   }
 
   Future<void> loadNext20Message() async {
+    if (reachEndList) return; // none to fetch
     // get list first 20 message by group id
+    setState(() {
+      onLoadMore = true;
+    });
     final fbMessages = await _inboxBloc.get20Messages(widget.group.id,
         lastMessageId: messages[0].id);
+    setState(() {
+      onLoadMore = false;
+    });
+    if (fbMessages.length < 20) {
+      // if return messages is smaller than 20 then it must have get everything
+      setState(() {
+        reachEndList = true;
+      });
+      if (fbMessages.length == 0) return; //if zero do not insert or setState
+    }
     messages.insertAll(
         0,
         fbMessages.map((element) {
@@ -159,119 +176,175 @@ class _InboxChatState extends State<InboxChat> {
         ],
       ),
       backgroundColor: Colors.grey[50],
-      body: LoadMoreScrollUp(
+      body: DashChat(
         scrollController: scrollController,
-        onLoadMore: () {},
-        list: DashChat(
-          scrollController: scrollController,
-          key: _chatViewKey,
-          inverted: false,
-          onSend: onSend,
-          sendOnEnter: true,
-          showLoadEarlierWidget: () => kLoadingSpinner,
-          textInputAction: TextInputAction.send,
-          user: users.firstWhere((user) => user.uid == _authBloc.userModel.id),
-          textCapitalization: TextCapitalization.sentences,
-          messageTextBuilder: (text, [messages]) {
-            return Padding(
-              padding: const EdgeInsets.all(3),
-              child: Text(
-                text,
-                style: ptBigBody(),
-              ),
-            );
-          },
-          messageTimeBuilder: (text, [messages]) {
-            return Text(
+        key: _chatViewKey,
+        inverted: false,
+        onSend: onSend,
+        sendOnEnter: true,
+
+        textInputAction: TextInputAction.send,
+        user: users.firstWhere((user) => user.uid == _authBloc.userModel.id),
+        textCapitalization: TextCapitalization.sentences,
+        messageTextBuilder: (text, [messages]) {
+          return Padding(
+            padding: const EdgeInsets.all(3),
+            child: Text(
               text,
-              style: ptSmall(),
-            );
-          },
-          inputToolbarPadding: EdgeInsets.all(4),
-          inputDecoration:
-              InputDecoration.collapsed(hintText: "Send message..."),
-          dateFormat: DateFormat('yyyy-MMM-dd'),
-          timeFormat: DateFormat('HH:mm'),
-          messages: messages,
-          showUserAvatar: false,
-          showAvatarForEveryMessage: false,
-          scrollToBottom: true,
-          onPressAvatar: (ChatUser user) {
-            print("OnPressAvatar: ${user.name}");
-          },
-          onLongPressAvatar: (ChatUser user) {
-            print("OnLongPressAvatar: ${user.name}");
-          },
-          inputMaxLines: 5,
-          messageContainerPadding: EdgeInsets.only(left: 10, right: 10),
-          alwaysShowSend: true,
-          inputTextStyle: TextStyle(fontSize: 16.0),
-          inputContainerStyle: BoxDecoration(
-            border: Border.all(width: 0.0),
-            color: Colors.white,
-          ),
-          onQuickReply: (Reply reply) {
-            // setState(() {
-            //   messages.add(ChatMessage(
-            //       text: reply.value, createdAt: DateTime.now(), user: user));
-
-            //   messages = [...messages];
-            // });
-
-            // Timer(Duration(milliseconds: 300), () {
-            //   _chatViewKey.currentState.scrollController
-            //     ..animateTo(
-            //       _chatViewKey
-            //           .currentState.scrollController.position.maxScrollExtent,
-            //       curve: Curves.easeOut,
-            //       duration: const Duration(milliseconds: 300),
-            //     );
-            // });
-          },
-          onLoadEarlier: () {
-            print("loading...");
-            loadNext20Message();
-          },
-          shouldShowLoadEarlier: false,
-          showTraillingBeforeSend: true,
-          inputFooterBuilder: () => _file != null
-              ? Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8),
-                  child: Row(
-                    children: [
-                      Container(
-                        margin: EdgeInsets.only(bottom: 8),
-                        padding: EdgeInsets.all(5),
-                        decoration: BoxDecoration(
-                            color: Colors.green[50],
-                            borderRadius: BorderRadius.circular(10)),
-                        child: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Icon(Icons.file_present),
-                            Text(
-                              path.basename(_file.path),
-                              style: ptBody().copyWith(
-                                color: Colors.black54,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                )
-              : SizedBox.shrink(),
-          trailing: <Widget>[
-            IconButton(
-              icon: Icon(Icons.file_present),
-              onPressed: () async {
-                imagePicker(context, (str) {}, (str) {});
-              },
-            )
-          ],
+              style: ptBigBody(),
+            ),
+          );
+        },
+        messageTimeBuilder: (text, [messages]) {
+          return Text(
+            text,
+            style: ptSmall(),
+          );
+        },
+        inputToolbarPadding: EdgeInsets.all(4),
+        inputDecoration: InputDecoration.collapsed(hintText: "Send message..."),
+        dateFormat: DateFormat('yyyy-MMM-dd'),
+        timeFormat: DateFormat('HH:mm'),
+        messages: messages,
+        showUserAvatar: false,
+        showAvatarForEveryMessage: false,
+        scrollToBottom: true,
+        onPressAvatar: (ChatUser user) {
+          print("OnPressAvatar: ${user.name}");
+        },
+        onLongPressAvatar: (ChatUser user) {
+          print("OnLongPressAvatar: ${user.name}");
+        },
+        inputMaxLines: 5,
+        messageContainerPadding: EdgeInsets.only(left: 10, right: 10),
+        alwaysShowSend: true,
+        inputTextStyle: TextStyle(fontSize: 16.0),
+        inputContainerStyle: BoxDecoration(
+          border: Border.all(width: 0.0),
+          color: Colors.white,
         ),
+        onQuickReply: (Reply reply) {
+          // setState(() {
+          //   messages.add(ChatMessage(
+          //       text: reply.value, createdAt: DateTime.now(), user: user));
+
+          //   messages = [...messages];
+          // });
+
+          // Timer(Duration(milliseconds: 300), () {
+          //   _chatViewKey.currentState.scrollController
+          //     ..animateTo(
+          //       _chatViewKey
+          //           .currentState.scrollController.position.maxScrollExtent,
+          //       curve: Curves.easeOut,
+          //       duration: const Duration(milliseconds: 300),
+          //     );
+          // });
+        },
+        // onLoadEarlier: () {
+        //   print("loading...");
+        //   loadNext20Message();
+        // },
+        shouldShowLoadEarlier: true,
+        showTraillingBeforeSend: true,
+        showLoadEarlierWidget: () {
+          if (!reachEndList)
+            return LoadEarlierWidget(
+              onLoadEarlier: () {
+                print("loading...");
+                loadNext20Message();
+              },
+              onLoad: onLoadMore,
+            );
+          else
+            return SizedBox.shrink();
+        },
+        inputFooterBuilder: () => _file != null
+            ? Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8),
+                child: Row(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(bottom: 8),
+                      padding: EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                          color: Colors.green[50],
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Icon(Icons.file_present),
+                          Text(
+                            path.basename(_file.path),
+                            style: ptBody().copyWith(
+                              color: Colors.black54,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            : SizedBox.shrink(),
+        trailing: <Widget>[
+          IconButton(
+            icon: Icon(Icons.file_present),
+            onPressed: () async {
+              imagePicker(context, (str) {}, (str) {});
+            },
+          )
+        ],
       ),
     );
+  }
+}
+
+class LoadEarlierWidget extends StatelessWidget {
+  const LoadEarlierWidget(
+      {Key key, @required this.onLoadEarlier, @required this.onLoad})
+      : super(key: key);
+
+  final Function onLoadEarlier;
+  final bool onLoad;
+
+  @override
+  Widget build(BuildContext context) {
+    return onLoad
+        ? SizedBox(
+            width: 30,
+            height: 30,
+            child: Center(child: CircularProgressIndicator()),
+          )
+        : GestureDetector(
+            onTap: () {
+              if (onLoadEarlier != null) {
+                onLoadEarlier();
+              }
+            },
+            child: Container(
+              padding: EdgeInsets.symmetric(
+                horizontal: 12.0,
+                vertical: 5.0,
+              ),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20.0),
+                  boxShadow: [
+                    BoxShadow(
+                      spreadRadius: 1.0,
+                      blurRadius: 5.0,
+                      color: Color.fromRGBO(0, 0, 0, 0.2),
+                      offset: Offset(0, 10),
+                    )
+                  ]),
+              child: Text(
+                "Load More",
+                style: TextStyle(
+                  color: Theme.of(context).primaryColor,
+                ),
+              ),
+            ),
+          );
   }
 }
