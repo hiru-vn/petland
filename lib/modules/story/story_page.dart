bbc8007e1b065b68a25496d9e0479aeb057e9c33
Nguyễn Quang Huy <quanghuy1998kh@gmail.com>
2021-01-29 06:54:49 +0700

    tab story

--- lib/modules/story/story_page.dart
+++ lib/modules/story/story_page.dart
@@ -12,6 +12,7 @@ class StoryPage extends StatefulWidget {
 class _StoryPageState extends State<StoryPage> {
   PostBloc _postBloc;
   PageController _pageController = PageController();
+  PageController _tabController = PageController();
   int _currentPage = 0;
   int _postCounter = 0;
 
@@ -44,22 +45,46 @@ class _StoryPageState extends State<StoryPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: ptDarkColor(context),
-      appBar: StoryAppbar(optionChange: (option) {},),
+      appBar: StoryAppbar(
+        optionChange: (option) {
+          if (option == StoryOptions.Popular)
+            _tabController.animateToPage(1,
+                duration: Duration(milliseconds: 200),
+                curve: Curves.decelerate);
+          if (option == StoryOptions.Following)
+            _tabController.animateToPage(0,
+                duration: Duration(milliseconds: 200),
+                curve: Curves.decelerate);
+        },
+      ),
       body: SafeArea(
         child: Stack(
           children: [
             Positioned(
-              top: 0,
-              left: 0,
-              right: 0,
-              bottom: 0,
-              child: PageView(
-                controller: _pageController,
-                scrollDirection: Axis.vertical,
-                children:
-                    _postBloc.posts.map((e) => StoryWidget(post: e)).toList(),
-              ),
-            ),
+                top: 0,
+                left: 0,
+                right: 0,
+                bottom: 0,
+                child: PageView(
+                    controller: _tabController,
+                    scrollDirection: Axis.horizontal,
+                    physics: NeverScrollableScrollPhysics(),
+                    children: [
+                      PageView(
+                        controller: _pageController,
+                        scrollDirection: Axis.vertical,
+                        children: _postBloc.posts
+                            .map((e) => StoryWidget(post: e))
+                            .toList(),
+                      ),
+                      PageView(
+                        controller: _pageController,
+                        scrollDirection: Axis.vertical,
+                        children: _postBloc.posts
+                            .map((e) => StoryWidget(post: e))
+                            .toList(),
+                      ),
+                    ])),
           ],
         ),
       ),
